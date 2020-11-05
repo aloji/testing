d7dca4d1c89d22f2c6f9a8287c8093043a7ec4a9
Alain <alain@fxstreet.com>
2020-11-05 20:19:55 +0100

    NSubstituteMock

---
+++ src/Aloji.AspNetCore.Testing.NSubstitute/Mocks/NSubstituteMock.cs
@@ -0,0 +1,20 @@
+﻿using NSubstitute.ClearExtensions;
+
+namespace Aloji.AspNetCore.Testing.Mocks
+{
+    public class NSubstituteMock<TService> : IMock<TService>
+       where TService : class
+    {
+        public TService Instance { get; private set; }
+
+        public NSubstituteMock()
+        {
+            this.Instance = NSubstitute.Substitute.For<TService>();
+        }
+
+        public virtual void Clear()
+        {
+            this.Instance.ClearSubstitute();
+        }
+    }
+}
